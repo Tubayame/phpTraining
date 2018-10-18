2ce6b32c444e5ae67d91d8ba5f1e5e6ad29898ca
Tubayame <42385500+Tubayame@users.noreply.github.com>
2018-10-18 17:55:12 +0900

    remove spaces

--- fizzBuzz.php
+++ fizzBuzz.php
@@ -2,15 +2,15 @@
 <?php
 
 function fizzBuzzQ() {
-    for ( $i=1; $i <= 1000; $i++ ){
-        if ( $i % 3 == 0 && $i % 5 == 0 ){
+    for ($i=1; $i <= 1000; $i++){
+        if ($i % 3 == 0 && $i % 5 == 0){
             echo 'Fizz Buzz';
-        } elseif($i % 3== 0){
+        } elseif ($i % 3 == 0){
             echo 'Fizz';
-        } elseif($i % 5 == 0){
+        } elseif ($i % 5 == 0){
             echo 'Buzz';
         } else echo $i;
-            echo ',';
+        echo ',';
     }
 }
 
